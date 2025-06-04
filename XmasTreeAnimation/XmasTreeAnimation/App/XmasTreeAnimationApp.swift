a863dd7ebed1ec56d005c7cfb1b52443bbfa8ba6
HiroDaegu <ds-3632@hanmail.net>
2025-06-04 14:33:10 +0900

    Create XMastree Animation

---
+++ XmasTreeAnimation/XmasTreeAnimation/App/XmasTreeAnimationApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct XmasTreeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XmasTreeAnimation/XmasTreeAnimation/View/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        XmasTreeView()
+    }
+}
+
+#Preview {
+    ContentView()
+        .preferredColorScheme(.dark)
+}
