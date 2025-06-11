0e91efa6fc5495ad963388b7652e62cae6cfdf2c
HiroDaegu <ds-3632@hanmail.net>
2025-06-11 13:41:09 +0900

    Create Circle Loader

---
+++ CircleLoader/CircleLoader/ContentView.swift
@@ -0,0 +1,75 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State var currentTrim = 0.2
+    @State var angleRotation = -90
+    @State var widthRipple1 = 250.0
+    @State var heightRipple1 = 250.0
+    @State var widthRipple2 = 260.0
+    @State var heightRipple2 = 260.0
+    @State var speedOfLoader = 0.02
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(style: StrokeStyle(lineWidth: 30))
+                .fill(
+                    Color(
+                        red: 0.51,
+                        green: 0.38,
+                        blue: 0.94,
+                        opacity: 0.55
+                    )
+                )
+                .frame(
+                    width: 200,
+                    height: 200
+                )
+                .overlay(
+                    Circle()
+                        .trim(
+                            from: 0,
+                            to: currentTrim
+                        )
+                        .stroke(style: StrokeStyle(lineWidth: 30))
+                        .fill(
+                            Color(
+                                red: 0.51,
+                                green: 0.38,
+                                blue: 0.94
+                            )
+                        )
+                        .frame(
+                            width: 200,
+                            height: 200
+                        )
+                        .rotationEffect(.init(degrees: Double(angleRotation)))
+                )
+            Circle()
+                .stroke(style: StrokeStyle(lineWidth: 30))
+                .fill(
+                    Color(
+                        red: 0.28,
+                        green: 0.05,
+                        blue: 0.66,
+                        opacity: 0.20
+                    )
+                )
+                .frame(
+                    width: 150,
+                    height: 150
+                )
+        }
+        .onAppear {
+            Timer.scheduledTimer(
+                withTimeInterval: speedOfLoader,
+                repeats: true
+            ) { timer in
+                angleRotation += 2
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
